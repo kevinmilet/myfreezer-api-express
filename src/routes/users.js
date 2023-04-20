fd021164ab5c2e42efc32d38aff0cc0bee8ff380
kevinmilet <kevin.milet@gmail.com>
2023-04-20 12:48:20 +0200

    ...

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 // Import des modules nécessaires
 const express = require('express');
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 // Récupération du router d'express
@@ -45,24 +46,120 @@ router.put('', (req, res) => {
 	User.findOne({ where: { email: email }, raw: true })
 		.then(user => {
 			if (user !== null) {
-				return res
-					.status(409)
-					.json({ message: `The user with email: ${email} already exists` });
+				return res.status(409).json({
+					message: `The user with email: ${data.email} already exists`,
+				});
 			}
 
-			User.create(req.body).then(user =>
-				res
-					.json({ message: 'User created', data: user })
-					.catch(err =>
-						res.status(500).json({ message: 'Database error', error: err })
-					)
-			);
+			bcrypt
+				.hash(password, parseInt(process.env.BCRYPT_SALT_ROUND))
+				.then(hash => {
+					data.password = hash;
+
+					const data = {
+						firstname: '',
+						lastname: '',
+						email: '',
+						password: '',
+						account_id: '',
+					};
+
+					let uuid = self.crypto.randomUUID().toString().replace('-', '');
+
+					data.firstname = firstname.trim();
+					data.lastname = lastname.trim();
+					data.email = email.trim();
+					data.account_id = uuid;
+
+					User.create(data).then(user =>
+						res
+							.json({ message: 'User created', data: user })
+							.catch(err =>
+								res.status(500).json({ message: 'Database error', error: err })
+							)
+					);
+				})
+				.catch(err => res.status(500).json({ message: 'Unknown error' }));
 		})
 		.catch(err =>
 			res.status(500).json({ message: 'Database error', error: err })
 		);
 });
 
-router.patch('/:id');
+router.patch('/:id', (req, res) => {
+	let userId = parseInt(req.params.id);
 
-router.delete('/:id');
+	if (!userId) {
+		return res.status(400).json({ message: 'Missing parameters' });
+	}
+
+	User.findOne({ where: { id: userId }, raw: true })
+		.then(user => {
+			if (user === null) {
+				return res.status(404).json({ message: 'User not found' });
+			}
+
+			const data = {
+				firstname: '',
+				lastname: '',
+				email: '',
+			};
+
+			data.firstname = firstname.trim();
+			data.lastname = lastname.trim();
+			data.email = email.trim();
+
+			User.update(data, { where: { id: userId } })
+				.then(user => res.json({ message: 'User updated', data: user }))
+				.catch(err =>
+					res.status(500).json({ message: 'Database error', error: err })
+				);
+		})
+		.catch(err =>
+			res.status(500).json({ message: 'Database error', error: err })
+		);
+});
+
+router.delete('/:id', (req, res) => {
+	let userId = parseInt(req.params.id);
+
+	if (!userId) {
+		return res.status(400).json({ message: 'Missing parameters' });
+	}
+
+	// Supression du user
+	User.destroy({ where: { id: userId }, force: true })
+		.then(() => res.status(204).json({}))
+		.catch(err =>
+			res.status(500).json({ message: 'Database error', error: err })
+		);
+});
+
+router.delete('/trash/:id', (req, res) => {
+	let userId = parseInt(req.params.id);
+
+	if (!userId) {
+		return res.status(400).json({ message: 'Missing parameters' });
+	}
+
+	// Soft delete du user
+	User.destroy({ where: { id: userId } })
+		.then(() => res.status(204).json({}))
+		.catch(err =>
+			res.status(500).json({ message: 'Database error', error: err })
+		);
+});
+
+router.post('/untrash/:id', (req, res) => {
+	let userId = parseInt(req.params.id);
+
+	if (!userId) {
+		return res.status(400).json({ message: 'Missing parameters' });
+	}
+
+	User.restore({ where: { id: userId } })
+		.then(() => res.status(204).json({}))
+		.catch(err =>
+			res.status(500).json({ message: 'Database error', error: err })
+		);
+});
